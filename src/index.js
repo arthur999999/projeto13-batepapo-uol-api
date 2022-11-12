18794f03eb41e7643e59914caba07dcd7ae50084
arthur999999 <arthurhono.contato@gmail.com>
2022-11-12 01:13:48 -0300

    rota post participantes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { MongoClient} from 'mongodb';
 import cors from "cors";
 import dotenv from 'dotenv';
 import joi from 'joi';
+import dayjs from 'dayjs';
 
 dotenv.config()
 
@@ -17,15 +18,42 @@ mongoClient.connect(()=> {
     db = mongoClient.db("UOL")
 })
 
-app.get('/test', async (req, res)=> {
+const nameSchamer = joi.object({
+    name: joi.string().required()
+})
+
+dayjs().format('mm')
+
+app.post('/participants', async (req, res)=> {
+    const userName = req.body
+
     try {
-        const nonee = await db.collection('mensagens').find().toArray()
-        res.send(nonee)
+        const sameName = await db.collection('participantes').find().toArray()
+        for(let element of sameName){
+            if(userName.name == element.name){
+                res.sendStatus(409)
+                return
+            }
+        }
     } catch (error) {
-        res.status(500).send(error)
+        res.status(422).send(error)
+    }
+
+    const validation = nameSchamer.validate(userName, { abortEarly: true} )
+
+    if (validation.error){
+        res.status(422).send(validation.error.details)
+        return
+    }
+
+    try {
+        await db.collection('participantes').insertOne({name: userName.name, lastStatus: Date.now()}), db.collection('mensagens').insertOne({from: userName.name , to: 'Todos', text: 'entra na sala...', type: 'status', time: `${(dayjs().hour() < 10 ? '0' + dayjs().hour() : dayjs().hour() )}:${(dayjs().minute() < 10 ? '0' + dayjs().minute() : dayjs().minute() )}:${(dayjs().second() < 10 ? '0' + dayjs().second() : dayjs().second() )}`})
+        res.sendStatus(201)
+    } catch (error) {
+        res.status(422).send(error)
     }
 })
 
-app.listen(5000, ()=> {
-    console.log('rodando na 5000')
+app.listen(process.env.PORT, ()=> {
+    console.log('rodando bipbop')
 })
